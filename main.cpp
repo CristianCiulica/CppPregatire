21da9699037d43dca078f17440b9353f7426fca1
pufulete-mic <dizomaria123@gmail.com>
2026-05-01 11:38:16 +0300

    problema suma linii matrice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
+int n,m,M[101][101],s,i,j;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    cin>>n>>m;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++)
+            cin>>M[i][j];
+    for (i=1;i<=n;i++) {
+        s=0;
+        for (j=1;j<=m;j++)
+            s+=M[i][j];
+        cout<<s<<' ';
+    }
+
     return 0;
 }
